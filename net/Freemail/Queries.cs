372b6808b6b9f16ea0b488e28dd9b3ea4723e7f1
Nick <nickmkk@gmail.com>
2015-05-01 16:41:52 -0500

    fixed an assembly directory path issue so this works in both a web app and normal class library

--- net/Freemail/Queries.cs
+++ net/Freemail/Queries.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Contexts;
 using System.Text.RegularExpressions;
+using System.Web.Configuration;
 
 namespace Freemail
 {
@@ -13,16 +16,25 @@ namespace Freemail
 		
 		static Queries()
 		{
-			var freePath = Environment.CurrentDirectory + "\\free.txt";
+			var baseDirectory = AssemblyDirectory();
+
+			var freePath = baseDirectory + "\\free.txt";
 			var freeContents = File.ReadAllText(freePath);
 			FreeDomains = Regex.Split(freeContents, "\r\n").ToList();
-			
-			var disposablePath = Environment.CurrentDirectory + "\\disposable.txt";
+
+			var disposablePath = baseDirectory + "\\disposable.txt";
 			var disposableContents = File.ReadAllText(disposablePath);
 			DisposableDomains = Regex.Split(disposableContents, "\r\n").ToList();
 			FreeDomains.AddRange(DisposableDomains);
 		}
 
+		static public string AssemblyDirectory()
+		{
+			string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+			var uri = new UriBuilder(codeBase);
+			string path = Uri.UnescapeDataString(uri.Path);
+			return Path.GetDirectoryName(path);
+		}
 
 		public static bool IsFree(string email)
 		{
