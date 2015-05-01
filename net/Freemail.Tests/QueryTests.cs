173e850bf737b44b056fd559aeb4ce69ce3b403f
Nick <nickmkk@gmail.com>
2015-05-01 10:17:15 -0500

    added a c# project for .net queries

---
+++ net/Freemail.Tests/QueryTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Freemail.Tests
+{
+	public class QueryTests
+	{
+
+		[TestCase("test@gmail.com", true)]
+		[TestCase("test@mailinater.com", true)]
+		[TestCase("test@randomdomain.com", false)]
+		public void IsFreeTests(string email, bool expectedResult)
+		{
+			Assert.AreEqual(Freemail.Queries.IsFree(email), expectedResult);
+		}
+
+		[TestCase("test@mailinater.com", true)]
+		[TestCase("test@gmail.com", false)]
+		public void IsDisposableTests(string email, bool expectedResult)
+		{
+			Assert.AreEqual(Freemail.Queries.IsDisposable(email), expectedResult);
+		}
+	}
+}
---
+++ net/Freemail/Queries.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Freemail
+{
+	public class Queries
+	{
+		private static List<string> FreeDomains { get; set; }
+		private static List<string> DisposableDomains { get; set; }
+		
+		static Queries()
+		{
+			var freePath = Environment.CurrentDirectory + "\\data\\free.txt";
+			var freeContents = File.ReadAllText(freePath);
+			FreeDomains = Regex.Split(freeContents, "\r\n").ToList();
+			
+			var disposablePath = Environment.CurrentDirectory + "\\data\\disposable.txt";
+			var disposableContents = File.ReadAllText(disposablePath);
+			DisposableDomains = Regex.Split(disposableContents, "\r\n").ToList();
+			FreeDomains.AddRange(DisposableDomains);
+		}
+
+
+		public static bool IsFree(string email)
+		{
+			var domain = email.Split('@').LastOrDefault();
+			return FreeDomains.Contains(domain);
+		}
+
+		public static bool IsDisposable(string email)
+		{
+			var domain = email.Split('@').LastOrDefault();
+			return DisposableDomains.Contains(domain);
+		}
+	}
+}
